5324da9b15c7cd2720fd4229aada54fbff81daf0
Erik Golinelli <erik@codekraft.it>
2023-04-02 13:44:49 +0200

    mapbox component root updated

--- src/components/Mapbox/index.tsx
+++ src/components/Mapbox/index.tsx
@@ -1,72 +1,22 @@
-import { useEffect, useRef } from '@wordpress/element';
-import mapboxgl from 'mapbox-gl';
+import Mapbox from './Map';
 import { MapboxSidebar } from './MapboxSidebar';
-import { Mapbox } from './Map';
-import { defaults } from '../../constants';
-import MapboxGeocoder from '@mapbox/mapbox-gl-geocoder';
 
-export default ( { attributes, mapInstance } ) => {
-	const {
-		latitude,
-		longitude,
-		pitch,
-		bearing,
-		mapZoom,
-		mapStyle,
-		mapboxOptions,
-	} = attributes;
-
-	const geocoderRef: React.MutableRefObject< MapboxGeocoder | undefined > =
-		useRef();
-
-	useEffect( () => {
-		if ( defaults?.accessToken ) {
-			mapboxgl.accessToken = defaults.accessToken;
-			if ( mapboxgl.accessToken && mapInstance.current !== null ) {
-				const map = new mapboxgl.Map( {
-					container: mapInstance.current || '',
-					style: 'mapbox://styles/mapbox/' + mapStyle,
-					center: [ parseFloat( longitude ), parseFloat( latitude ) ],
-					pitch: parseFloat( pitch ),
-					bearing: parseFloat( bearing ),
-					zoom: parseFloat( mapZoom ),
-				} );
-			}
-		} else {
-			console.error( 'cannot find access token' );
-		}
-	}, [] );
-
-	useEffect( () => {
-		if ( defaults?.accessToken ) {
-			if ( mapboxgl.accessToken && mapInstance.current !== null ) {
-				const currentMap = new mapboxgl.Map( {
-					container: mapInstance.current,
-					style: 'mapbox://styles/mapbox/' + mapStyle,
-					center: [ parseFloat( longitude ), parseFloat( latitude ) ],
-					pitch: parseFloat( pitch ),
-					bearing: parseFloat( bearing ),
-					zoom: parseFloat( mapZoom ),
-				} );
-			}
-		} else {
-			throw new Error( 'cannot find access token' );
-		}
-	}, [ defaults ] );
-
+export default ( { attributes, mapContainer, map, defaults } ) => {
 	return (
 		<div className="map-wrapper">
-			{ mapboxOptions.sidebarEnabled ? (
+			{ attributes.sidebarEnabled ? (
 				<MapboxSidebar
+					defaults={ defaults }
 					map={ map }
-					geocoderEnabled={ mapboxOptions.geocoderEnabled }
-					geocoderRef={ geocoderRef }
-					mapboxOptions={ mapboxOptions }
-					listings={ mapboxOptions.listings }
+					geocoderRef={ attributes.geocoderRef }
+					mapboxOptions={ attributes.mapboxOptions }
 				/>
 			) : null }
 			<div id="map-container">
-				<Mapbox { ...mapboxOptions } />
+				<Mapbox
+					attributes={ attributes }
+					mapContainer={ mapContainer }
+				/>
 			</div>
 		</div>
 	);
