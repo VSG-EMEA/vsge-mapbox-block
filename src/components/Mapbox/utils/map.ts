80ff1c6a9c2d85a7f10b89610935d05586c52bac
Erik Golinelli <erik@codekraft.it>
2023-04-24 22:38:52 +0200

    enhanced map init (wait th main map to load --eventually-- the other layers)

--- src/components/Mapbox/utils/map.ts
+++ src/components/Mapbox/utils/map.ts
@@ -4,9 +4,9 @@ import mapboxgl from 'mapbox-gl';
  * The function initializes a Mapbox map with specified attributes and adds a terrain layer if
  * specified.
  *
- * @param {HTMLElement} mapRef The HTML element that will contain the map.
- * @param {Object}      attributes   An object containing various attributes for initializing the map, including
- *                                   latitude, longitude, pitch, bearing, mapZoom, mapStyle, and treeDimensionality.
+ * @param {HTMLElement} mapRef     The HTML element that will contain the map.
+ * @param {Object}      attributes An object containing various attributes for initializing the map, including
+ *                                 latitude, longitude, pitch, bearing, mapZoom, mapStyle, and treeDimensionality.
  * @return {mapboxgl.Map} a mapboxgl.Map object.
  */
 export function initMap( mapRef, attributes ) {
@@ -22,27 +22,29 @@ export function initMap( mapRef, attributes ) {
 		pitch,
 	} );
 
-	if ( attributes.treeDimensionality ) {
-		if ( ! map.getSource( 'mapbox-dem' ) ) {
-			map.addSource( 'mapbox-dem', {
-				type: 'raster-dem',
-				url: 'mapbox://mapbox.mapbox-terrain-dem-v1',
-				tileSize: 512,
-				maxzoom: 14,
-			} );
-		}
+	map.on( 'load', function () {
+		if ( map && attributes.treeDimensionality ) {
+			if ( ! map.getSource( 'mapbox-dem' ) ) {
+				map.addSource( 'mapbox-dem', {
+					type: 'raster-dem',
+					url: 'mapbox://mapbox.mapbox-terrain-dem-v1',
+					tileSize: 512,
+					maxzoom: 14,
+				} );
+			}
 
-		// add the DEM source as a terrain layer with exaggerated height
-		map.setTerrain( {
-			source: 'mapbox-dem',
-			exaggeration: 1.5,
-		} );
-	} else if (
-		! attributes.treeDimensionality &&
-		map.getSource( 'mapbox-dem' )
-	) {
-		map.removeSource( 'mapbox-dem' );
-	}
+			// add the DEM source as a terrain layer with exaggerated height
+			map.setTerrain( {
+				source: 'mapbox-dem',
+				exaggeration: 1.5,
+			} );
+		} else if (
+			! attributes.treeDimensionality &&
+			map.getSource( 'mapbox-dem' )
+		) {
+			map.removeSource( 'mapbox-dem' );
+		}
+	} );
 
 	return map;
 }
