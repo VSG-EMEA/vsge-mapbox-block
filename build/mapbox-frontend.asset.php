304654dbcc56fde48a08c76dda31bf1980c2a5fb
Erik Golinelli <erik@codekraft.it>
2023-04-25 11:32:23 +0200

    removes the build folder from repository

--- build/mapbox-frontend.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('wp-i18n'), 'version' => 'dd1895133a5d9ff69b36');
--- build/mapbox.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '3671dbafd481e5b978f8');
