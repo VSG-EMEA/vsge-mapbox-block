030cb63385f647452e156c9b55a96fc5fe14a164
Erik Golinelli <erik@codekraft.it>
2023-05-04 21:20:58 +0200

    catch the position of the click then update the lngLat value

--- src/components/Mapbox/index.tsx
+++ src/components/Mapbox/index.tsx
@@ -1,40 +1,112 @@
 import { Map } from './Map';
 import { Sidebar } from './Sidebar';
 import { TopBar } from './TopBar';
-import { useEffect, useContext, useState } from '@wordpress/element';
+import { useEffect, useContext } from '@wordpress/element';
 import { MapboxContext } from './MapboxContext';
 import { initMap } from './utils/initMap';
-import mapboxgl from 'mapbox-gl';
+import mapboxgl, {
+	MapboxGeoJSONFeature,
+	MapMouseEvent,
+	Point,
+} from 'mapbox-gl';
+import { pin } from '@wordpress/icons';
 import { initGeocoder } from './utils/geocoder';
-import { MountedMapsContextValue } from '../../types';
-import { MarkerPopup } from './Popup';
-import { initMarkers } from './utils/markers';
+import { MapAttributes, MountedMapsContextValue } from '../../types';
+import { addMarker, addMarkers } from './Markers';
+import { addPopup, removePopup } from './Popup';
 
-export function MapBox( { attributes } ): JSX.Element {
+export function tempMarker( id: number = 0, coordinates = [ 0, 0 ] ) {
+	return {
+		type: 'Feature',
+		id,
+		properties: {
+			name: 'temp',
+			icon: pin,
+			color: 'green',
+		},
+		geometry: {
+			type: 'Point',
+			coordinates,
+		},
+	};
+}
+
+function removeMarkerById(
+	id: number,
+	markersList: MapboxGeoJSONFeature[]
+): MapboxGeoJSONFeature[] {
+	return markersList.filter( ( marker ) => marker.id !== id );
+}
+
+function getMarkerData(
+	id: number,
+	markersList: mapboxgl.MapboxGeoJSONFeature[]
+) {
+	return markersList.find( ( marker ) => marker.id === id );
+}
+
+export function MapBox( {
+	attributes,
+}: {
+	attributes: MapAttributes;
+} ): JSX.Element {
 	const {
 		map,
 		setMap,
 		setGeoCoder,
-    setLngLat,
 		defaults,
 		mapRef,
 		geocoderRef,
+		setLngLat,
+		setMarkers,
+		markers,
 	}: MountedMapsContextValue = useContext( MapboxContext );
 
-	function listenForClick( map ) {
+	function removeTempMarkers(
+		mapRef: React.RefObject< HTMLDivElement > | undefined
+	) {
+		if ( mapRef?.current ) {
+			mapRef?.current
+				.querySelectorAll( '.marker-temp' )
+				.forEach( ( marker ) => marker.remove() );
+		}
+	}
+
+	function listenForClick( map: mapboxgl.Map ) {
 		if ( map ) {
-			map.on( 'click', ( e ) => {
-				const labels = e.features?.map( ( feature ) => (
-					<MarkerPopup
-						key={ feature.properties?.id }
-						{ ...feature.properties }
-					/>
-				) );
-
-				console.log( e.lngLat );
-
-				// setPopupContent( labels );
+			map.on( 'click', ( e: MapMouseEvent ) => {
+				// store the last clicked position
 				setLngLat( e.lngLat );
+
+				const bbox = [
+					[ e.point.x - 0.0005, e.point.y - 0.0005 ],
+					[ e.point.x + 0.0005, e.point.y + 0.0005 ],
+				];
+				// Find features intersecting the bounding box.
+				// const selectedFeatures = map.queryRenderedFeatures( bbox );
+				const clickedEl = e.originalEvent.target.parentElement;
+
+				if ( markers && clickedEl?.nodeName === 'BUTTON' ) {
+					if ( clickedEl.dataset?.markerName === 'temp' ) {
+						return addPopup( map, {} );
+					}
+					const thisMarker = getMarkerData(
+						Number( clickedEl.dataset?.id ),
+						markers
+					);
+
+					return addPopup( map, thisMarker );
+				}
+
+				removeTempMarkers( mapRef );
+
+				addMarker(
+					tempMarker( markers?.length, [
+						e.lngLat.lng,
+						e.lngLat.lat,
+					] ),
+					map
+				);
 			} );
 		}
 	}
@@ -46,6 +118,9 @@ export function MapBox( { attributes } ): JSX.Element {
 
 			// Initialize map and store the map instance
 			setMap( initMap( mapRef.current, attributes, defaults ) );
+
+			// Add the stored listings to the markers list
+			setMarkers( attributes.mapboxOptions.listings );
 		} else {
 			console.log( 'No access token' );
 		}
@@ -54,21 +129,27 @@ export function MapBox( { attributes } ): JSX.Element {
 	useEffect( () => {
 		if ( map ) {
 			map.on( 'load', () => {
-				// Add markers
-				initMarkers( attributes.mapboxOptions.listings, map );
-
 				// Add geocoder
 				if ( attributes.sidebarEnabled && attributes.geocoderEnabled ) {
 					setGeoCoder(
 						initGeocoder( geocoderRef, map, attributes, defaults )
 					);
 				}
-
-				// Listen for marker clicks
-				listenForClick( map );
 			} );
 		}
 	}, [ map ] );
+
+	useEffect( () => {
+		if ( map && markers?.length ) {
+			map.on( 'load', () => {
+				// add markers to the map
+				addMarkers( markers, map );
+
+				// highlight the first listing
+				listenForClick( map );
+			} );
+		}
+	}, [ markers ] );
 
 	return (
 		<div
@@ -76,10 +157,7 @@ export function MapBox( { attributes } ): JSX.Element {
 			style={ { minHeight: attributes.mapHeight } }
 		>
 			{ attributes.sidebarEnabled ? (
-				<Sidebar
-					attributes={ attributes }
-					geocoderRef={ geocoderRef }
-				/>
+				<Sidebar attributes={ attributes } />
 			) : null }
 			<div className={ 'map-container' }>
 				<TopBar { ...attributes } />
