a039edc96c0bf21a8364d3fff51c2641a011f6d4
Erik Golinelli <erik@codekraft.it>
2023-09-13 10:36:12 +0200

    marker styles (pin + geocoder)

--- src/components/Mapbox/defaults.tsx
+++ src/components/Mapbox/defaults.tsx
@@ -1,27 +1,31 @@
 // Default marker style
 import { DefaultMarker } from './Pin';
 import { __ } from '@wordpress/i18n';
+import { MapBoxListing } from '../../types';
+import { LngLatLike } from 'mapbox-gl';
 
 export const defaultMarkerSize: number = 48;
 
+export const defaultColors: string[] = [ '#004a83', '#ce0e2d', '#44f' ];
+
 export const defaultMarkerStyle: JSX.Element = DefaultMarker( {
-	color: '#f44',
+	color: defaultColors[ 0 ],
 	size: defaultMarkerSize,
 } );
 
 export const tempMarkerStyle: JSX.Element = DefaultMarker( {
-	color: '#4f4',
+	color: defaultColors[ 1 ],
 	size: defaultMarkerSize,
 } );
 
 export const geoMarkerStyle: JSX.Element = DefaultMarker( {
-	color: '#44f',
+	color: defaultColors[ 2 ],
 	size: defaultMarkerSize,
 } );
 
 export const customMarkerStyle = (
 	children: JSX.Element,
-	color: 'blue',
+	color: string,
 	size: number
 ) =>
 	DefaultMarker( {
@@ -38,7 +42,7 @@ export const defaultMarkerProps = {
 	city: '',
 	cap: '',
 	iconSize: defaultMarkerSize,
-	iconColor: '#FF0000',
+	iconColor: defaultColors[ 0 ],
 	itemTags: [],
 	itemFilters: [],
 };
@@ -47,25 +51,48 @@ export const defaultMarkerProps = {
  * This is a TypeScript function that creates a temporary marker with specified properties and
  * coordinates.
  *
- * @param {number | undefined}   id          - The id parameter is an optional number that represents the unique
- *                                           identifier for the temporary marker feature. If no id is provided, it defaults to undefined.
- * @param {number[] | undefined} coordinates - The `coordinates` parameter is an array of two numbers
- *                                           representing the longitude and latitude of a point on a map. It is used to specify the location of
- *                                           the marker.
+ * @param {number | undefined}     id          - The id parameter is an optional number that represents the unique
+ *                                             identifier for the temporary marker feature. If no id is provided, it defaults to undefined.
+ * @param {LngLatLike | undefined} coordinates - The `coordinates` parameter is an array of two numbers
+ *                                             representing the longitude and latitude of a point on a map. It is used to specify the location of
+ *                                             the marker.
  * @return A function that returns a GeoJSON feature object representing a temporary marker with
  * specified properties and coordinates. The function takes two optional parameters: `id` (a number
  * representing the marker's unique identifier) and `coordinates` (an array of two numbers representing
  * the marker's longitude and latitude).
  */
-export function tempMarker( id: number, coordinates: number[] ) {
+export function generateTempMarkerData(
+	id: number,
+	coordinates: LngLatLike | [ number, number ]
+): MapBoxListing {
 	return {
 		type: 'Feature',
 		id,
 		properties: {
 			name: 'temp',
-			icon: tempMarkerStyle,
+			icon: 'pin',
 			iconSize: defaultMarkerSize,
-			iconColor: '#1a1',
+			iconColor: defaultColors[ 1 ],
+		},
+		geometry: {
+			type: 'Point',
+			coordinates,
+		},
+	};
+}
+
+export function generateGeocoderMarkerData(
+	id: number,
+	coordinates: LngLatLike | [ number, number ]
+): MapBoxListing {
+	return {
+		type: 'Feature',
+		id,
+		properties: {
+			name: 'geocoder',
+			icon: 'geocoder',
+			iconSize: defaultMarkerSize,
+			iconColor: defaultColors[ 2 ],
 		},
 		geometry: {
 			type: 'Point',
