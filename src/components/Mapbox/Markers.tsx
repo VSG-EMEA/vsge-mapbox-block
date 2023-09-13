a35dcd5ad8202c4dd37092bb85b907d7c4057377
Erik Golinelli <erik@codekraft.it>
2023-09-13 10:39:03 +0200

    enhancing the function to add a marker to the map

--- src/components/Mapbox/Markers.tsx
+++ src/components/Mapbox/Markers.tsx
@@ -2,8 +2,10 @@ import { RefObject } from 'react';
 import { createRef, createRoot } from '@wordpress/element';
 import mapboxgl, { LngLatLike } from 'mapbox-gl';
 import { Marker } from './Marker';
-import { DefaultMarker } from './Pin';
-import { MarkerItem } from '../../types';
+import { DefaultMarker, PinPoint } from './Pin';
+import { MapBoxListing, MarkerItem } from '../../types';
+import { areValidCoordinates } from '../Sortable/utils';
+import { safeSlug } from '../../utils';
 
 /**
  * This function adds markers to a map based on a GeoJSON object.
@@ -31,26 +33,74 @@ export function addMarkers(
  *                            including its geometry and properties.
  * @param              map    - The `map` parameter is an instance of the `mapboxgl.Map` class, which represents a
  *                            Mapbox map. It is used to add the marker to the map and set its position.
+ * @param              type
  */
 export function addMarker(
-	marker: MarkerItem,
+	marker: MapBoxListing,
 	map: mapboxgl.Map
 ): mapboxgl.Marker | undefined {
 	if ( marker?.geometry ) {
-		const ref: RefObject< HTMLDivElement > = createRef();
+		const ref: RefObject< HTMLElement > = createRef< HTMLElement >();
+
 		// Create a new DOM root and save it to the React ref
 		ref.current = document.createElement( 'div' );
+		ref.current.className =
+			'marker marker-' + safeSlug( marker.properties.name );
 		const root = createRoot( ref.current );
+
+		console.log(marker.properties.icon)
+
+		let markerIcon = null;
+		switch ( marker.properties.icon ) {
+			case Number( marker.properties.icon ):
+				markerIcon = (
+					<DefaultMarker
+						color={ marker.properties.iconColor }
+						size={ marker.properties.iconSize }
+					/>
+				);
+				break;
+			case 'geocoder':
+			case 'pin':
+				markerIcon = (
+					<PinPoint
+						color={ marker.properties.iconColor }
+						size={ marker.properties.iconSize }
+					/>
+				);
+				break;
+			case 'default':
+			default:
+				markerIcon = (
+					<DefaultMarker
+						color={ marker.properties.iconColor }
+						size={ marker.properties.iconSize }
+					/>
+				);
+				break;
+		}
+
 		// Render a Marker Component on our new DOM node
-		root.render( <Marker feature={ marker } map={ map } /> );
+		root.render(
+			<Marker feature={ marker } map={ map } children={ markerIcon } />
+		);
 
 		// Add markers to the map at all points
-		return new mapboxgl.Marker( ref.current, {
-			offset: [ 0, ( marker?.properties?.iconSize || 0 ) * -0.5 ],
-		} )
-			.setLngLat(
-				( marker?.geometry?.coordinates as LngLatLike ) || [ 0, 0 ]
-			)
-			.addTo( map );
+		return areValidCoordinates(
+			marker?.geometry?.coordinates as [ number, number ]
+		)
+			? new mapboxgl.Marker( ref.current, {
+					offset: [ 0, ( marker?.properties?.iconSize || 0 ) * -0.5 ],
+			  } )
+					.setLngLat(
+						( marker?.geometry?.coordinates as LngLatLike ) || [
+							0, 0,
+						]
+					)
+					.addTo( map )
+			: console.log(
+					'Invalid coordinates for marker ',
+					marker.properties.country
+			  );
 	}
 }
