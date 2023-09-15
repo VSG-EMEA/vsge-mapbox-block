532a90e3b0a1693fb47b5ccee7933bb28521fa5d
Erik Golinelli <erik@codekraft.it>
2023-09-15 11:24:06 +0200

    enableListing from sidebar the click event

--- src/components/Mapbox/Listing.tsx
+++ src/components/Mapbox/Listing.tsx
@@ -3,23 +3,23 @@ import { mapMarker } from '@wordpress/icons';
 import { MapBoxListing, MarkerProps } from '../../types';
 import mapboxgl from 'mapbox-gl';
 import { TagList } from './TagItem';
+import { enableListing } from '../../utils/dataset';
 
 /**
  * This is a TypeScript React component that renders a listing based on the type of property passed in.
  *
- * @param {Object}       jsonFeature
- * @param {Object}       jsonFeature.jsonFeature
- * @param {mapboxgl.Map} jsonFeature.map
- * @param {Function}     jsonFeature.onClick
+ * @param                p
+ * @param {Object}       p.jsonFeature
+ * @param {mapboxgl.Map} p.map
  * @return A React component that renders a listing of properties if the type is 'Feature', and
  * returns null otherwise. The listing includes the name, phone, and address of the property.
  */
 export const Listing = ( {
 	jsonFeature,
+	map,
 }: {
 	jsonFeature: MapBoxListing;
-	map?: mapboxgl.Map | null;
-	onClick?: Function;
+	map: mapboxgl.Map;
 } ) => {
 	const {
 		properties: {
@@ -38,7 +38,7 @@ export const Listing = ( {
 	return (
 		<div className={ 'mapbox-sidebar-feature listing' }>
 			<Icon icon={ mapMarker } />
-			<div>
+			<button onClick={ () => enableListing( map, jsonFeature ) }>
 				<TagList
 					tags={ itemFilters }
 					className={ 'sidebar-filter-list' }
@@ -87,7 +87,7 @@ export const Listing = ( {
 						/>
 					</>
 				) : null }
-			</div>
+			</button>
 		</div>
 	);
 };
