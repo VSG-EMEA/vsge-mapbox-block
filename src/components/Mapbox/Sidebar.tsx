fe7f130cd92537ac1b0f3802e57ca540ae84fe4a
Erik Golinelli <erik@codekraft.it>
2023-09-16 10:41:49 +0200

    dynamically shows the selected listing in the sidebar

--- src/components/Mapbox/Sidebar.tsx
+++ src/components/Mapbox/Sidebar.tsx
@@ -2,25 +2,37 @@ import { Listing } from './Listing';
 import { GeoCoder } from './Geocoder';
 import mapboxgl from 'mapbox-gl';
 import { MapBoxListing, MountedMapsContextValue } from '../../types';
-import { useContext } from '@wordpress/element';
+import { useContext, useState } from '@wordpress/element';
 import { MapboxContext } from './MapboxContext';
+import { filterListings } from '../../utils/view';
 
 /**
  * This is a TypeScript React component that renders a list of MapBox listings with a click event
  * handler.
  *
  * @param props
- * @param props.listings An object with a property "listings" which is an array of MapBoxListing objects.
- * @param props.map      The `map` parameter is an instance of the Mapbox GL JS map object, which is used to
- *                       display and interact with the map on the web page. It is passed as a parameter to the `Listings`
- *                       component so that it can be used by the child `Listing` components to interact with the map.
+ * @param props.listings         An object with a property "listings" which is an array of MapBoxListing objects.
+ * @param props.map              The `map` parameter is an instance of the Mapbox GL JS map object, which is used to
+ *                               display and interact with the map on the web page. It is passed as a parameter to the `Listings`
+ *                               component so that it can be used by the child `Listing` components to interact with the map.
+ * @param props.filteredListings An array of listing IDs that are currently enabled on the map
  * @return JSX.Element 	 A React component that renders a list of `Listing` components based on the `listings` prop
  *                       passed to it, along with a `map` and `onClick` function.
  */
-function Listings( props: { listings: MapBoxListing[]; map: mapboxgl.Map } ) {
+function Listings( props: {
+	listings: MapBoxListing[];
+	map: mapboxgl.Map;
+	filteredListings: number[] | null;
+} ) {
+	console.log( 'filtered items', props.filteredListings );
+	const tempListings = filterListings(
+		props.listings,
+		props.filteredListings
+	);
+
 	return (
 		<div className={ 'feature-listing' }>
-			{ props.listings.map( ( data, index: number ) => (
+			{ tempListings.map( ( data, index: number ) => (
 				<Listing key={ index } jsonFeature={ data } map={ props.map } />
 			) ) }
 		</div>
@@ -41,7 +53,7 @@ function Listings( props: { listings: MapBoxListing[]; map: mapboxgl.Map } ) {
  */
 export const Sidebar = ( { attributes } ): JSX.Element => {
 	const { geocoderEnabled, mapboxOptions } = attributes;
-	const { geocoderRef, map }: MountedMapsContextValue =
+	const { geocoderRef, map, filteredListings }: MountedMapsContextValue =
 		useContext( MapboxContext );
 
 	return (
@@ -49,7 +61,13 @@ export const Sidebar = ( { attributes } ): JSX.Element => {
 			{ geocoderEnabled ? (
 				<GeoCoder geocoderRef={ geocoderRef } />
 			) : null }
-			<Listings listings={ mapboxOptions.listings } map={ map } />
+			{ map ? (
+				<Listings
+					listings={ mapboxOptions.listings }
+					map={ map }
+					filteredListings={ filteredListings }
+				/>
+			) : null }
 		</div>
 	);
 };
