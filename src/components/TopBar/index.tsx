ccdb9064c666beaaeaab879dcd30e80d90cbe5c3
Erik Golinelli <erik@codekraft.it>
2023-10-23 13:12:51 +0200

    adds the reset filter icon

--- src/components/TopBar/index.tsx
+++ src/components/TopBar/index.tsx
@@ -1,8 +1,14 @@
 import { Button, Icon, SelectControl } from '@wordpress/components';
 import { useEffect, useState } from '@wordpress/element';
 import { filterListingsBy, fitInView } from '../../utils/view';
-import { FilterCollection, MapboxOptions, MountedMapsContextValue } from '../../types';
+import {
+	FilterCollection,
+	MapboxOptions,
+	MountedMapsContextValue,
+} from '../../types';
 import { useMapboxContext } from '../Mapbox/MapboxContext';
+import { __ } from '@wordpress/i18n';
+import { clearListingsDistances } from '../../utils/spatialCalcs';
 
 /**
  * trasform an array of strings into a select values that could be used with select control
@@ -31,6 +37,21 @@ const centerViewIcon = () => (
 			>
 				<path d="M0 0h48v48H0z" fill="none" />
 				<path d="M10 30H6v8a4 4 0 0 0 4 4h8v-4h-8v-8zm0-20h8V6h-8a4 4 0 0 0-4 4v8h4v-8zm28-4h-8v4h8v8h4v-8a4 4 0 0 0-4-4zm0 32h-8v4h8a4 4 0 0 0 4-4v-8h-4v8zM24 16a8 8 0 1 0 0 16 8 8 0 0 0 0-16zm0 12a4 4 0 1 1 0-8 4 4 0 0 1 0 8z" />
+			</svg>
+		) }
+	/>
+);
+
+const resetIcon = () => (
+	<Icon
+		icon={ () => (
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				height="24"
+				viewBox="0 -960 960 960"
+				width={'24'}
+			>
+				<path d="M440-122q-121-15-200.5-105.5T160-440q0-66 26-126.5T260-672l57 57q-38 34-57.5 79T240-440q0 88 56 155.5T440-202v80Zm80 0v-80q87-16 143.5-83T720-440q0-100-70-170t-170-70h-3l44 44-56 56-140-140 140-140 56 56-44 44h3q134 0 227 93t93 227q0 121-79.5 211.5T520-122Z" />
 			</svg>
 		) }
 	/>
@@ -76,6 +97,19 @@ export const TopBar = ( attributes: {
 					: 'map-topbar-hidden'
 			}
 		>
+			{ filteredListings.length > 0 ? (
+				<Button
+					icon={ resetIcon }
+					isSmall={ true }
+					className={ 'reset-filters' }
+					onClick={ () => {
+						setFilteredListings( [] );
+						clearListingsDistances( listings );
+					} }
+				>
+					{ __( 'Reset Filters' ) }
+				</Button>
+			) : null }
 			{ fitView ? (
 				<Button
 					icon={ centerViewIcon }
@@ -83,7 +117,7 @@ export const TopBar = ( attributes: {
 					className={ 'fit-view' }
 					onClick={ () => fitInView( map, listings, mapRef ) }
 				>
-					fit-view
+					{ __( 'Fit View' ) }
 				</Button>
 			) : null }
 
