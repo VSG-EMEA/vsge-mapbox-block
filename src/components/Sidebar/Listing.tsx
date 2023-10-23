a04a25814ee86f68d5085ad592a02cf66d165666
Erik Golinelli <erik@codekraft.it>
2023-10-23 13:12:30 +0200

    updated sidebar listing

--- src/components/Sidebar/Listing.tsx
+++ src/components/Sidebar/Listing.tsx
@@ -1,11 +1,70 @@
 import { Icon } from '@wordpress/components';
 import { mapMarker } from '@wordpress/icons';
-import { MapBoxListing } from '../../types';
+import { MapBoxListing, SearchMarkerProps } from '../../types';
 import mapboxgl from 'mapbox-gl';
 import { TagList } from '../TagItem';
-import { removePopups } from '../Popup/Popup';
+import { addPopup, removePopups } from '../Popup/Popup';
 import { RefObject } from 'react';
-import { enableListing } from '../Mapbox/utils';
+import { __ } from '@wordpress/i18n';
+import { flyToStore } from '../../utils/view';
+import { PopupContent, SearchPopup } from '../Popup/PopupContent';
+
+/**
+ * The function highlights a specific feature in a listing by adding a CSS class to it and removing the
+ * class from any previously active feature.
+ *
+ * @param {MapBoxListing} item - The `item` parameter is of type `Feature`, which is likely an object
+ *                             representing a geographic feature on a map. It may contain properties such as the feature's ID,
+ *                             coordinates, and other attributes. The function `highlightListing` is using this parameter to
+ *                             identify a specific feature and highlight its corresponding
+ */
+export function highlightListing( item: MapBoxListing ) {
+	document.getElementById( 'feature-listing' )?.classList.add( 'filtered' );
+
+	const activeItem = document.getElementsByClassName( 'active-store' );
+	if ( activeItem[ 0 ] ) {
+		activeItem[ 0 ].classList.remove( 'active-store' );
+	}
+
+	if ( item.properties ) {
+		const listing = document.getElementById( 'marker-' + item.id );
+		listing?.classList.add( 'active-store' );
+	}
+}
+
+/**
+ * Enables the listing feature on the map.
+ *
+ * @param {mapboxgl.Map}  map    - The map object.
+ * @param {MapBoxListing} marker - The listing marker object.
+ */
+export function enableListing( map: mapboxgl.Map, marker: MapBoxListing ) {
+	console.log( 'Listing enabled', marker );
+
+	// 1. Fly to the point
+	flyToStore( map, marker );
+
+	const getPopupTemplate = (
+		marker: MapBoxListing
+	): JSX.Element | undefined => {
+		if ( marker.type === 'Feature' ) {
+			return <PopupContent { ...marker.properties } />;
+		} else if ( marker.properties.name === 'geocoder' ) {
+			return (
+				<SearchPopup
+					{ ...( marker.properties as SearchMarkerProps ) }
+				/>
+			);
+		}
+		return null;
+	};
+
+	// 2. Close all other popups and display popup for clicked store
+	// addPopup( map, marker, getPopupTemplate( marker ) );
+
+	// 3. Highlight listing in sidebar (and remove highlight for all other listings)
+	highlightListing( marker );
+}
 
 /**
  * This is a TypeScript React component that renders a listing based on the type of property passed in.
@@ -38,6 +97,7 @@ export const Listing = ( {
 			itemFilters,
 			emailAddress,
 			website,
+			distance,
 		},
 	} = jsonFeature;
 	return (
@@ -68,7 +128,8 @@ export const Listing = ( {
 				<p>
 					{ address && address }
 					<br />
-					{ country && country } - { city && city } ({ countryCode })
+					{ country && country } { city && city }{ ' ' }
+					{ countryCode && '(' + countryCode + ')' }
 				</p>
 
 				{ website && (
@@ -88,6 +149,11 @@ export const Listing = ( {
 						>
 							{ emailAddress }
 						</a>
+					</p>
+				) }
+				{ distance && (
+					<p className={ 'store-distance' }>
+						{ __( 'Distance: ' ) + distance.toFixed( 1 ) + 'Km' }
 					</p>
 				) }
 				{ itemTags?.length ? (
