68657a642f14c0a762826f68252a7eb46dd9f44b
Erik Golinelli <erik@codekraft.it>
2023-11-17 13:17:21 +0100

    generates the pot file automatically with wp-babel-makepot

--- babel.config.js
+++ babel.config.js
@@ -1,10 +1,16 @@
-module.exports = ( api ) => {
-  api.cache.using( () => process.env.NODE_ENV );
+module.exports = (api) => {
+  api.cache.using(() => process.env.NODE_ENV);
 
   return {
     presets: [
       '@babel/preset-typescript',
       '@wordpress/babel-preset-default',
     ],
+    plugins: [
+      [
+        "@wordpress/babel-plugin-makepot",
+        {"output": "languages/vsge-mapbox-block.pot"}
+      ]
+    ]
   };
 };
