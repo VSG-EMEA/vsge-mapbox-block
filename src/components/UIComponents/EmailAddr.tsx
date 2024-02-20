6883c92e468092a69ecd737759e4c86f65b0e2eb
Erik Golinelli <erik@codekraft.it>
2024-02-20 13:37:27 +0100

    adds the EmailAddr component

---
+++ src/components/UIComponents/EmailAddr.tsx
@@ -0,0 +1,16 @@
+export const EmailAddr = ( {
+	emailAddress,
+	label,
+	className,
+}: {
+	emailAddress: string;
+	label: string;
+	className: string;
+} ) => {
+	if ( ! emailAddress ) return null;
+	return (
+		<p className={ className }>
+			{ label }: <a href={ 'mailto:' + emailAddress }>{ emailAddress }</a>
+		</p>
+	);
+};
