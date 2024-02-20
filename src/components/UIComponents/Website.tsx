56f61f37282f46ae55c3b1cd6cff0be27b53a8eb
Erik Golinelli <erik@codekraft.it>
2024-02-20 13:37:50 +0100

    adds the Website component

---
+++ src/components/UIComponents/Website.tsx
@@ -0,0 +1,16 @@
+export const Website = ( {
+	websiteUri,
+	text,
+	className,
+}: {
+	websiteUri: string;
+	text: string;
+	className: string;
+} ) => {
+	if ( ! websiteUri ) return null;
+	return (
+		<p className={ className }>
+			<a href={ '//' + websiteUri }>{ text }</a>
+		</p>
+	);
+};
