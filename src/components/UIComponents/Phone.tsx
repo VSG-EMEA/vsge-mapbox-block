264a7c73a716a87da1e336096eb828a7cd458378
Erik Golinelli <erik@codekraft.it>
2024-02-20 13:37:39 +0100

    adds the Phone component

---
+++ src/components/UIComponents/Phone.tsx
@@ -0,0 +1,18 @@
+export const Phone = ( {
+	phone,
+	label,
+	className,
+}: {
+	phone: string;
+	label: string;
+	className: string;
+} ) => {
+	if ( ! phone ) {
+		return null;
+	}
+	return (
+		<p className={ className }>
+			{ label }: <a href={ 'tel:' + phone }>{ phone }</a>
+		</p>
+	);
+};
