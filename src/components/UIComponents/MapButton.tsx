c46916ace79b767465b5bad6427575ce65dca84f
Erik Golinelli <erik@codekraft.it>
2024-02-21 10:40:49 +0100

    MapButton new component

---
+++ src/components/UIComponents/MapButton.tsx
@@ -0,0 +1,32 @@
+/**
+ * Create a map button component.
+ *
+ * @param {Object} props - The props for the MapButton component
+ * @param {() => JSX.Element} props.icon - The icon to display
+ * @param {boolean} props.hidden - Whether the button should be hidden
+ * @param {string} props.className - The class name for the button
+ * @param {() => void} props.onClick - The function to call when the button is clicked
+ * @param {JSX.Element | string} props.children - The children to display
+ *
+ * @return {JSX.Element} The map button component
+ */
+export const MapButton = ( props: {
+	icon: () => JSX.Element;
+	hidden?: boolean;
+	className?: string;
+	onClick: () => void;
+	children: JSX.Element | string;
+} ): JSX.Element => {
+	const { icon, hidden, className, onClick, children } = props;
+
+	return (
+		<button
+			onClick={ onClick }
+			className={ 'map-button ' + className }
+			hidden={ hidden }
+		>
+			{ icon() }
+			{ children }
+		</button>
+	);
+};
