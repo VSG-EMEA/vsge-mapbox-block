fcdb62501afdd3ae3943b0ff1ec55e38730f32ab
Erik Golinelli <erik@codekraft.it>
2024-02-28 14:04:59 +0100

    updated links fields
    close #20

--- src/components/UIComponents/LinkTo.tsx
+++ src/components/UIComponents/LinkTo.tsx
@@ -1,10 +1,10 @@
 /**
  * Creates a link to the specified website URI with the given text and optional class name.
  *
- * @param {Object} props
- * @param {string} props.websiteUri - The URI of the website to link to.
- * @param {string=} props.text - The text to display for the link.
- * @param {string=} props.className - An optional class name for styling the link.
+ * @param {Object}  props
+ * @param {string}  props.websiteUri - The URI of the website to link to.
+ * @param {string=} props.text       - The text to display for the link.
+ * @param {string=} props.className  - An optional class name for styling the link.
  * @return {JSX.Element | null} The link element or null if the website URI is not provided.
  */
 export const LinkTo = ( {
@@ -18,7 +18,18 @@ export const LinkTo = ( {
 } ): JSX.Element | null => {
 	return text ? (
 		<b className={ 'mbb-link ' + className }>
-			{ websiteUri ? <a href={ '//' + websiteUri }> { text } </a> : text }
+			{ websiteUri ? (
+				<a
+					href={ '//' + websiteUri }
+					title={ text }
+					target={ '_blank' }
+					rel="noreferrer noopener"
+				>
+					{ text }
+				</a>
+			) : (
+				text
+			) }
 		</b>
 	) : null;
 };
