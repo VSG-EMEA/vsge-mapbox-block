fbc96be13a58a2adccd4d2c29ae8bf3310cc3431
Erik Golinelli <erik@codekraft.it>
2024-07-04 13:39:31 +0200

    Displays the popup banner "find a reseller" at the first click on the map

--- src/components/Mapbox/index.tsx
+++ src/components/Mapbox/index.tsx
@@ -174,6 +174,23 @@ export function MapBox( {
 
 				setUpdatedPin( [ newTempMarker ] );
 				setListings( newListings );
+
+				const popupRef = createRef< HTMLDivElement | null >();
+				addPopup(
+					map.current,
+					newTempMarker,
+					popupRef.current,
+					<PinPointPopup
+						location={ clickedPoint }
+						listings={ listings }
+						setListings={ setListings }
+						setFilteredListings={ setFilteredListings }
+						mapRef={ mapRef.current }
+						map={ map.current }
+						mapDefaults={ mapDefaults }
+						markersRef={ markersRef.current }
+					/>
+				);
 				return;
 			}
 
