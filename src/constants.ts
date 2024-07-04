8a7375ed303cb239ecc656c696f932efd55104f5
Erik Golinelli <erik@codekraft.it>
2024-07-04 13:18:18 +0200

    geocoder now allows to locate at address level

--- src/constants.ts
+++ src/constants.ts
@@ -17,7 +17,7 @@ export const DEFAULT_COLOR_ALT = '#004a83';
 export const ICON_SIZE: number = 36;
 // https://docs.mapbox.com/api/search/geocoding/#data-types
 export const DEFAULT_GEOCODER_TYPE_SEARCH: string =
-	'country,region,district,postcode,place,locality,poi';
+	'country,region,district,postcode,place,neighborhood,locality,poi,address';
 
 /**
  * Markers
