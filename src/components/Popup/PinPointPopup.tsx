5cef2e54480cce1f5032f4cae424303c02931d48
Erik Golinelli <erik@codekraft.it>
2024-07-04 12:19:36 +0200

    (same as geocoder) optimized search for resellers

--- src/components/Popup/PinPointPopup.tsx
+++ src/components/Popup/PinPointPopup.tsx
@@ -1,11 +1,21 @@
 import type mapboxgl from 'mapbox-gl';
-import { CoordinatesDef, MapBoxListing } from '../../types';
+import {
+	CoordinatesDef,
+	MapboxBlockDefaults,
+	MapBoxListing,
+} from '../../types';
 import { __ } from '@wordpress/i18n';
-import { MARKER_TYPE_TEMP } from '../../constants';
-import { locateNearestStore } from '../../utils/spatialCalcs';
-import { removePopups, showNearestStore } from './index';
+import {
+	DEFAULT_GEOCODER_TYPE_SEARCH,
+	MARKER_TYPE_TEMP,
+} from '../../constants';
+import { removePopups } from './index';
 import type { Dispatch } from 'react';
-import { Result } from '@mapbox/mapbox-gl-geocoder';
+import MapboxGeocoder from '@mapbox/mapbox-gl-geocoder';
+import { getNearestStore } from '../Geocoder/utils';
+import { getTheCurrentTempPin } from '../../utils/view';
+import { initGeoMarker } from '../Marker/Geomarker';
+import { getNextId } from '../../utils/dataset';
 
 export function PinPointPopup( props: {
 	map: mapboxgl.Map;
@@ -14,8 +24,18 @@ export function PinPointPopup( props: {
 	listings: MapBoxListing[] | null;
 	setListings: Dispatch< MapBoxListing[] >;
 	setFilteredListings: Dispatch< MapBoxListing[] | null >;
+	mapDefaults: MapboxBlockDefaults;
+	markersRef: HTMLButtonElement[];
 } ): JSX.Element | null {
-	const { location, map, mapRef, listings, setFilteredListings } = props;
+	const {
+		location,
+		map,
+		mapRef,
+		listings,
+		setFilteredListings,
+		mapDefaults,
+		markersRef,
+	} = props;
 
 	if ( ! mapRef ) {
 		return null;
@@ -28,46 +48,67 @@ export function PinPointPopup( props: {
 			</h3>
 			<div className={ 'mapbox-popup-newpin-buttons' }>
 				<button
-					onClick={ () => {
-						// get the current temp pin data
-						const currentPinData = listings?.find( ( listing ) => {
-							return listing.type === MARKER_TYPE_TEMP;
+					onClick={ ( e ) => {
+						const currentPinData = getTheCurrentTempPin( listings );
+
+						const geo = new MapboxGeocoder( {
+							accessToken: mapDefaults.accessToken,
+							reverseGeocode: true,
+							language: mapDefaults.language || 'en',
+							types: DEFAULT_GEOCODER_TYPE_SEARCH,
 						} );
-						if ( ! currentPinData ) {
-							// eslint-disable-next-line no-console
-							console.error( 'currentPinData not found' );
-							return;
-						}
-						// sort the array and get the nearest store
-						const sortedNearestStores = locateNearestStore(
-							location,
-							listings
-						);
-						// create a new temp pin
-						const myLocationPin = {
-							...currentPinData,
-							text: __( 'My location', 'vsge-mapbox-block' ),
-							geometry: {
-								type: 'Point',
-								coordinates: location as CoordinatesDef,
-							},
-							properties: {
-								icon: 'myLocation',
-								draggable: false,
-								name: __( 'My location', 'vsge-mapbox-block' ),
-							},
-						};
-						const sortedListings = showNearestStore(
-							myLocationPin as Result & MapBoxListing,
-							sortedNearestStores,
-							mapRef,
-							map
-						);
-						const filtered = [ ...sortedListings, myLocationPin ];
-						setFilteredListings( filtered );
+
+						map.addControl( geo );
+
+						geo.query( `${ location[ 1 ] },${ location[ 0 ] }` );
+
+						// Add geocoder result to container.
+						geo.on( 'result', ( e ) => {
+							const marker = initGeoMarker(
+								getNextId( listings ),
+								markersRef
+							);
+
+							const searchResult =
+								e.result as MapboxGeocoder.Result;
+
+							// create a new temp pin
+							const myLocationPin: MapBoxListing = {
+								id: 0,
+								type: MARKER_TYPE_TEMP,
+								text: searchResult.place_name,
+								marker: marker,
+								place_name:
+									currentPinData.place_name ??
+									__( 'My location', 'vsge-mapbox-block' ),
+								geometry: {
+									type: 'Point',
+									coordinates: searchResult.geometry
+										.coordinates as CoordinatesDef,
+								},
+								properties: {
+									...currentPinData?.properties,
+								},
+							};
+
+							const filtered = getNearestStore(
+								searchResult,
+								mapRef,
+								map,
+								listings,
+								myLocationPin
+							);
+
+							map.removeControl( geo );
+
+							setFilteredListings( filtered );
+						} );
 					} }
 				>
-					{ __( 'Find the nearest store?', 'vsge-mapbox-block' ) }
+					{ __(
+						'Find the nearest Sales Agent',
+						'vsge-mapbox-block'
+					) }
 				</button>
 				<button
 					onClick={ () => {
